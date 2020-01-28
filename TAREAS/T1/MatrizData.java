8d85e9e77a0c9520eb852d0b38f0017cc5675bbf
Edin Montenegro <edinfusion@gmail.com>
2020-01-28 17:20:14 -0600

    Tareas

---
+++ TAREAS/T1/MatrizData.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author edinf
+ */
+import java.util.Scanner;
+public class MatrizData {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int m;
+        Scanner Sn = new Scanner(System.in);
+        System.out.println("Ingresar tamaño de matriz");
+        m = Sn.nextInt();
+        for (int i = 0; i <= m-1 ; i++) {
+             System.out.print("[1]"); 
+                for (int j = 1; j <= m-1; j++){
+                    if(i==0 || i==m-1 || j==m-1){
+                        System.out.print("[1]");
+                    }
+                    else {
+                        System.out.print("[ ]");
+                    }
+                    
+                }  
+               System.out.println();
+        }
+    }
+}
